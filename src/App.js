d66418928aecb2727983b852cb19b90a00acab3b
DDev16 <dillondevera12@gmail.com>
2023-08-28 12:45:13 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PaymentComponent from './PaymentComponent.js'; // Adjust the path as needed
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PayPal Payment Gateway Integration</h1>
+        <p className="payment-info">
+You are enlisting the expertise of Dillon Devera for professional Graphic Design services. This comprehensive package entails the creation of a custom logo for Angela Morrison. Drawing inspiration from commercial airline aesthetics, Dillon will work closely with you to deliver a distinctive and captivating design that aligns with your vision.
+Throughout the process, you'll have the opportunity to participate in a personalized, one-on-one design consultation. This collaborative approach ensures that the final design reflects your preferences and resonates with your brand identity.
+
+Upon completion, you will receive the logo design in a range of formats that guarantee its adaptability across various mediums. These formats include:
+
+High-resolution vector files (PDF) for pristine scalability.
+Raster image files (JPEG, PNG, TIFF) that are suitable for both digital and print applications.
+Additionally, this package encompasses post-design support, enabling you to make future adjustments, implement fixes, or request edits to the logo. This adaptability ensures that the logo remains a steadfast representation of your evolving brand.
+
+Experience the fusion of creativity and professionalism as Dillon Devera brings your brand's essence to life through an emblem that draws inspiration from the sleek designs of commercial airlines.
+
+</p>
+        <p className="payment-amount">$150</p>
+        <PaymentComponent />
       </header>
+      <main>
+        
+        
+      </main>
     </div>
   );
 }
---
+++ src/PaymentComponent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import './App.css';
+
+const PaymentComponent = () => {
+  const clientId = 'AYp_Tlz1y_8LHEqsunsOKfIMEBbxsLt3uao1CqNwJMVzt6e2mHwTKH6_n_sZcypXvEaUHGYsJkHItsCG';
+
+  return (
+    <PayPalScriptProvider options={{ 'client-id': clientId }}>
+      <div className="payment-container">
+        <h2 className="payment-header">Make a Payment</h2>
+        <PayPalButtons
+          className="paypal-button"
+          createOrder={(data, actions) => {
+            return actions.order.create({
+              purchase_units: [{
+                amount: {
+                  value: '150.00', // Adjust the payment amount if needed
+                }
+              }]
+            });
+          }}
+        />
+      </div>
+    </PayPalScriptProvider>
+  );
+};
+
+export default PaymentComponent;
